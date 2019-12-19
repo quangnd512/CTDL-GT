11455154e6035255b769e4dd1af1d0f0f9cc357e
quangnd512 <quangnd512@gmail.con>
2019-12-19 12:27:25 +0700

    code 19/12/2019

--- Danh-sach-lien-ket/Danh-sach-lien-ket-don.cpp
+++ Danh-sach-lien-ket/Danh-sach-lien-ket-don.cpp
@@ -5,18 +5,64 @@
 struct LinkedList{
 	int data; 
 	struct LinkedList *next; //Khoi tao con tro
-	// *nextla con tro cua chinh no va tro toi 1 node ke tiep cung co kieu tuong tu kieu cua *next
+	// *next la con tro cua chinh no va tro toi 1 node ke tiep cung co kieu tuong tu kieu cua *next
 };
-
-
 typedef struct LinkedList *node; //Kieu du lieu LinkedList duoc thay the bang node
+
 
 //Tao 1 note
 node CreateNode(int value){
 	node temp; //Khai bao 1 node
-	temp = (node)malloc(sizeof())
+	temp = (node)malloc(sizeof(struct LinkedList)) //Cap phat vung nho malloc()
+	/*
+	malloc = new trong huong doi tuong
+	*/
+	/*
+	Su dung malloc:
+		Khi su dung malloc phai tính toán kích thuoc vung nho can cap phat truoc roi truyen vao cho malloc
+		Khi su dung calloc chi can truyen vào so phan tu va kich thuoc 1 phan tu, thi calloc se tu dong tinh toan va cap phat vung nho can thiet
+	
+	Vi du: Cap phat mang 10 phan tu kieu int:
+		int *a = (int *) malloc( 10 * sizeof( int ));
+		int *b = (int *) calloc( 10, sizeof( int ));
+	*/
+	/*
+	Su an toan:
+		Su dung calloc an toan hon malloc vi sau khi khoi tao vùng nho thi calloc se khoi tao vung nho cap phat = 0,
+		con vung nho do malloc cap phat vung chua gia tri rac nen se de gay ra loi neu truy xuat toi vung nho nay truoc khi gan cho no mot gia tri xac dinh.
+	*/
+	
+	//sizeof la ham tra ve kich thuoc cua kieu du lieu, dung lam tham so cho ham malloc
+	temp->next = NULL; //Cho next tro toi null
+	temp->data = value; //Gan gia tri cho Node
+	return temp; //Tra ve gia tri
 }
 
 
+//Them node vao dau danh sach
+/*
+Huong giai quyet:
+Viec them vao dau chinh la viec cap nhat lai thang head. Ta goi Node moi(temp), ta co:
+	Neu head dang tro toi NULL, nghia la linked list dang trong, Node moi them vao se làm head luôn
+	Nguoc lai, ta phai thay the thang head cu bang head moi. Viec nay phai lam theo thu tu nhu sau:
+		Cho next cua temp tro toi head hien hanh
+		Ðat temp lam head moi
+*/
+node AddHead(node head, int value){
+    node temp = CreateNode(value); // Khoi tao node temp voi data = value
+    if(head == NULL){
+        head = temp; // //Neu linked list dang trong thi Node temp la head luon
+    }else{
+        temp->next = head; // Tro next cua temp = head hien tai
+        head = temp; // Ðoi head hien tai = temp(Vi temp bay gio là head moi)
+    }
+    return head;
+}
 
+
+//Them vao cuoi
+/*
+Huong giai quyet:
+
+*/
 
--- README.md
+++ README.md
@@ -58,14 +58,49 @@ VD: [Bài toán hà nội](https://github.com/quangnd512/CTDL-GT/blob/master/Tha
 - Danh sách liên kết  
 	Gồm 2 thành phần:  
 		Nút: chứa dữ liệu  
-		Con trỏ:  để trỏ tới nút kế tiếp  
+		Con trỏ: để trỏ tới nút kế tiếp  
 		
 - Có 4 loại danh sách liên kết:  
 	+ DS liên kết đơn:
 	+ DS liên kết kép:
 	+ DS liên kết vòng đơn:
-	+ DS liên kết vòng kép:
+	+ DS liên kết vòng kép:  
+	<img src="https://imgur.com/fc5Szfb">  
+	Một danh sách thường cói 2 phần là phần danh sách chứa dữ liệu và vần liên kết chứa con trỏ
 	
-## Ngăn xếp (Stack)
+- Ứng dụng của danh sách là công 2 đa thức  
 
+
+## Ngăn xếp (Stack)
+- Ngăn xếp là dạng đặc biệt của danh sách mà việc thêm hay loại bỏ đều ở trên đầu ngăn xếp  
+
+- Đặc tả của ngăn xếp:  
+	Đặc tả dữ liệu:  
+		Có nhiều nút và cùng 1 kiểu dữ liệu  
+		Có đỉnh stack  
+	Đặc tả các task vụ trên stack:  
+		Initialize:  
+			Chức năng: Khởi động stack  
+			Dữ liệu nhập: Không  
+			Dữ liệu xuất: stack top về vị trí ban đầu  
+		Empty:
+			Chức năng: Kiểm tra stack có rỗng hay không  
+			Dữ liệu nhập: Không  
+			Dữ liệu xuất: True or False  
+		Push:  
+		Pop:  
+		Stacktop:  
+		Stacksize:  
+		Clearstack:  
+		Copystack:  
+		
+		Đặc điểm của stack:  
+		 + Che dấu thông tin  
+		 + Tính khả thi và hiệu quả của ứng dụng  
+		 + Tính trong sáng của chương trình  
+		 + Thiết kế từ trên xuống  
+
+- Cài đặt stack
+	+ Cài đặt ngăn xếp bằng mảng  
+	+ Cài đặt ngăn xếp bằng danh sách liên kết  
 	
